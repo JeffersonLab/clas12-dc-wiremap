da7a2a4e3b0c95e2f5ba537e50a5adc1f6cd8e95
bbyrd1111 <brook.byrd.13@cnu.edu>
2015-05-26 07:31:02 -0400

    crates tab does not load correctly

---
+++ three.py
@@ -0,0 +1,74 @@
+from __future__ import print_function, division
+
+from numpy import random as rand
+from PyQt4 import QtGui, uic
+
+def fn():
+    return rand.randint(0,7)
+
+class Sidebar(QtGui.QWidget):
+    def __init__(self,parent=None):
+        super(QtGui.QWidget, self).__init__(parent)
+        uic.loadUi('Sidebar.ui',self)  
+    def update_parameters(self):
+        pass 
+class dbtabwidget(QtGui.QTabWidget):
+    def __init__(self, parent=None):
+        super(QtGui.QWidget, self).__init__(parent)
+        uic.loadUi('dbtabwidget.ui',self)
+        
+# object constructor class
+class cratestabwidget(QtGui.QTabWidget):
+    def __init__(self, parent=None):
+        #Setting the inheritance
+        super(QtGui.QWidget, self).__init__(parent)
+        # loading the specific ui
+        uic.loadUi('cratestabwidget.ui', self)
+    def update_parameters(self):
+        pass
+  
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        uic.loadUi('MainWindow.ui',self)
+        
+        #creating an instance of each object
+        self.sidebar = Sidebar()
+        self.dboard = dbtabwidget()
+        self.crate = cratestabwidget()
+
+        vbox = QtGui.QVBoxLayout()
+        vbox.addWidget(self.sidebar)
+        vbox.addStretch(1)
+
+        newbox = QtGui.QVBoxLayout()     
+        newbox.addWidget(self.dboard)
+        newbox.addStretch(1)
+        
+        # create a vertical box that will contain the object instance
+        cratebox = QtGui.QVBoxLayout()
+        cratebox.addWidget(self.crate)
+        cratebox.addStretch(1)
+ 
+        # attach the respective layouts to their designated spots in the MainWindow.ui
+        self.sidebar_holder.setLayout(vbox)
+        self.distrib_board_tab_holder.setLayout(newbox)
+        self.crates_tab_holder.setLayout(cratebox)
+
+        self.show()
+        self.updating = False
+
+    def update_parameters(self):
+        if not self.updating:
+            self.updating = True
+            self.item1value.setValue(fn())
+            self.item2value.setValue(fn())
+            self.item1info.setText('fixed? '+str(fn()))
+            self.item2info.setText('fixed? '+str(fn()))
+            self.updating = False
+
+if __name__ == '__main__':
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    main_window = MainWindow()
+    sys.exit(app.exec_())
