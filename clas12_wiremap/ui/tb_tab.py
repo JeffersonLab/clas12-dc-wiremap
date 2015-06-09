309e9fcd719326a46e6309cebc5b6c1dcd39fd9a
SArah <sarah.clark.14@cnu.edu>
2015-06-09 15:48:01 -0400

    starting signals for tb_tab

--- clas12_wiremap/ui/tb_tab.py
+++ clas12_wiremap/ui/tb_tab.py
@@ -10,6 +10,88 @@ class TBTab(QtGui.QTabWidget):
         super(QtGui.QTabWidget, self).__init__(parent)
         curdir = os.path.dirname(os.path.realpath(__file__))
         uic.loadUi(os.path.join(curdir,'TBTab.ui'), self)
+        
+    def init_buttons(self):
+
+        sc_fmt = 'sc{sector}'
+        sl_fmt = 'sc{sector}_sl{superlyr}'
+        b_fmt = 'sc{sector}_sl{superlyr}_b{board}'
+        slot_fmt = 'sc{sector}_sl{superlyr}_b{board}_{slot}'
+
+        self.sector = []
+        self.superlyr = []
+        self.board = []
+        self.slot = []
+
+        for sector_id in range(1,7):
+            fmt = dict(sector=sector_id)
+
+            self.sectors.append(getattr(self,sc_fmt.format(**fmt)))
+            self.superlyrs.append([])
+            self.boards.append([])
+            self.slots.append([])
+            
+            sl_ids = range(1,7)
+
+            for sl_id in sl_ids:
+                fmt.update(superlyr=sl_id)
+
+                self.superlyrs[-1].append(getattr(self,sl_fmt.format(**fmt)))
+                self.boards[-1].append([])
+                self.slots[-1].append([])
+
+                b_ids = range(1,8)
+
+                for b_id in b_ids:
+                    fmt.update(board=b_id)
+
+                    self.boards[-1][-1].append(getattr(self,ss_fmt.format(**fmt)))
+                    self.slots[-1][-1].append([])
+                    
+                    if b_id < 6:
+                        slot_id = range(1,3)
+                    else :
+                        slot_id = range(1,2)
+
+                    for slotz_id in slot_id:
+                        fmt.update(slot=slotz_id)
+
+                        self.slots[-1][-1][-1].append(getattr(self,slot_fmt.format(**fmt)))
+
+        for sc_id,sc in enumerate(self.sectors): 
+
+            superlyrs = self.superlyrs[sc_id]
+            def _sc(_,sc=sc,sls=superlyrs):
+                slotkd = any([b.isChecked() for b in sls])
+                sc.setChecked(slotkd)
+
+            for sl_id,sl in enumerate(superlyrs):
+                sc.clicked.connect(sl.setChecked)
+                sl.clicked.connect(_sc)
+
+                board = self.boards[sc_id][sl_id]
+                def _sl(_,sl=sl,slotss=boards):
+                    slotkd = any([s.isChecked() for s in slotss])
+                    sl.setChecked(slotkd)
+
+                for b_id,b in enumerate(board):
+                    sl.clicked.connect(b.setChecked)
+                    sc.clicked.connect(b.setChecked)
+                    b.clicked.connect(_sl)
+                    b.clicked.connect(_sc)
+
+                    slotsss = self.channels[sc_id][sl_id][b_id]
+                    def _b(_,b=b,slots=slotsss):
+                        slotkd = any([c.isChecked() for c in slots])
+                        b.setChecked(slotkd)
+
+                    for ch in channels:
+                        b.clicked.connect(slot.setChecked)
+                        sl.clicked.connect(slot.setChecked)
+                        sc.clicked.connect(slot.setChecked)
+                        slot.clicked.connect(_b)
+                        slot.clicked.connect(_sl)
+                        slot.clicked.connect(_sc)
 
     def get_buttons(self):
 
