ecff00a8eb21b6a16090c059112b4d42498e1dc3
SArah <sarah.clark.14@cnu.edu>
2015-07-07 15:10:42 -0400

    made ui through python with checkable feature - still need to signal through the array

--- clas12_wiremap/ui/dpin_tab.py
+++ clas12_wiremap/ui/dpin_tab.py
@@ -11,19 +11,42 @@ class DoubletPinTab(QtGui.QTabWidget):
         self.init_ui()
 
     def init_ui(self):
-        for sec in range(1,7):
+    
+        self.superlayer = []
+        self.box = []
+        self.quad = []
+        self.doublet = []
+        self.pin = []
+        
+    
+        for sec in range(6):
             sector_tab = QtGui.QTabWidget()
             sector_layout = QtGui.QHBoxLayout()
             sector_tab.setLayout(sector_layout)
-            for slyr in range(1,7):
+            
+            self.superlayer.append([])
+            self.box.append([])
+            self.quad.append([])
+            self.doublet.append([])
+            self.pin.append([])
+            
+            for slyr in range(6):
                 superlayer_tab = QtGui.QWidget()
                 superlayer_layout = QtGui.QGridLayout()
 
                 # sector/superlayer
-                superlayer_button = QtGui.QPushButton('Sector {}, Superlayer {}'.format(sec,slyr))
-
+                superlayer_button = QtGui.QPushButton('Sector {}, Superlayer {}'.format(sec+1,slyr+1))
+                
+                self.superlayer[-1].append(superlayer_button)
+                self.box[-1].append([])
+                self.quad[-1].append([])
+                self.doublet[-1].append([])
+                self.pin[-1].append([])
+                              
+                superlayer_button.setCheckable(True) 
+                            
                 superlayer_layout.addWidget(superlayer_button, 0, 0, 1, 7)
-
+                
                 for b,box_name in enumerate(['Forward','Backward']):
 
                     # direction/box
@@ -31,20 +54,41 @@ class DoubletPinTab(QtGui.QTabWidget):
 
                     box_button.setSizePolicy(QtGui.QSizePolicy.Minimum,
                                              QtGui.QSizePolicy.Preferred)
+                                             
+                    self.box[-1][-1].append(box_button)
+                    self.quad[-1][-1].append([])
+                    self.doublet[-1][-1].append([])
+                    self.pin[-1][-1].append([])
+                    
+                    box_button.setCheckable(True) 
+                                            
                     superlayer_layout.addWidget(box_button,5*b+1,0,5,1)
-
+                    
+                    
                     for q in range(3):
 
                         # quad
                         quad_button = QtGui.QPushButton('Quad {}'.format(q+1))
+                        
+                        self.quad[-1][-1][-1].append(quad_button)
+                        self.doublet[-1][-1][-1].append([])
+                        self.pin[-1][-1][-1].append([])
 
+                        quad_button.setCheckable(True) 
+                        
                         superlayer_layout.addWidget(quad_button, 5*b+1, q*2+1, 1, 2)
 
+                        
                         for d in range(2):
 
                             # doublet
                             doublet_button = QtGui.QPushButton('Doublet {}'.format(d+1))
-
+                            
+                            self.doublet[-1][-1][-1][-1].append(doublet_button)
+                            self.pin[-1][-1][-1][-1].append([])
+                            
+                            doublet_button.setCheckable(True) 
+                            
                             superlayer_layout.addWidget(doublet_button, 5*b+2, q*2+d+1)
 
                             pin_grid_widget = QtGui.QWidget()
@@ -54,19 +98,23 @@ class DoubletPinTab(QtGui.QTabWidget):
                                 y = pin // 3
 
                                 # pin
-                                pin_widget = QtGui.QRadioButton('')
+                                pin_button = QtGui.QRadioButton('')
 
+                                self.pin[-1][-1][-1][-1][-1].append(pin_button)
+                                
+                                pin_button.setCheckable(True) 
+                                
                                 pin_layout.setContentsMargins(9,9,9,0)
-                                pin_layout.addWidget(pin_widget,y,x)
+                                pin_layout.addWidget(pin_button,y,x)
                             pin_grid_widget.setLayout(pin_layout)
                             superlayer_layout.addWidget(pin_grid_widget, 5*b+3, q*2+d+1)
 
                 superlayer_layout.setRowStretch(11,1)
                 superlayer_layout.setColumnStretch(7,1)
 
-                sector_tab.addTab(superlayer_tab, 'Superlayer {}'.format(slyr))
+                sector_tab.addTab(superlayer_tab, 'Superlayer {}'.format(slyr+1))
                 superlayer_tab.setLayout(superlayer_layout)
-            self.addTab(sector_tab, 'Sector {}'.format(sec))
+            self.addTab(sector_tab, 'Sector {}'.format(sec+1))
 
 
 if __name__ == '__main__':
